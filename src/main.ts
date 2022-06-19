40eb5be0902c7ad64f0a04915cd68fc05d637828
leehj050211 <leehj050211@gmail.com>
2022-06-19 22:18:08 +0900

    Add: 보안 설정

--- src/main.ts
+++ src/main.ts
@@ -3,9 +3,21 @@ dotenv.config({path: '.env'});
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import helmet from 'helmet';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  // 보안 설정
+  app.use(helmet.dnsPrefetchControl());
+  app.use(helmet.expectCt());
+  app.use(helmet.frameguard());
+  app.use(helmet.hidePoweredBy());
+  app.use(helmet.hsts());
+  app.use(helmet.ieNoOpen());
+  app.use(helmet.noSniff());
+  app.use(helmet.permittedCrossDomainPolicies());
+  app.use(helmet.referrerPolicy());
+  app.use(helmet.xssFilter());
   app.enableCors();
   app.useGlobalPipes(
     new ValidationPipe({
